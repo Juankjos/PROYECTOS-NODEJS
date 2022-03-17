a33dd3c3cc67b585e1e827a2a0d2e6473d98897a
Juankjos <juanca171097@gmail.com>
2022-03-16 19:15:25 -0600

    Búsqueda funcional, falta historial

--- CLIMA/index.js
+++ CLIMA/index.js
@@ -13,15 +13,28 @@ const main = async()=>{
 
         switch(opt){
             case 1:
+
                 const termino = await leerInput('Ciudad: ');
                 const lugares = await busquedas.ciudad(termino);
+                
+                //Selecciona el lugar y muestra una lista
+                //It choose the place showing a list
                 const id = await listplaces(lugares);
                 const placeCho = lugares.find(l => l.id === id);
 
+                //Con la selección del lugar, muestra el clima que tiene
+                //Choosing the place, shows the weather it has
+                const clima = await busquedas.weatherPlace(placeCho.lat, placeCho.lng);
+
+                console.clear();
                 console.log('\nINFORMACIÓN DE LA CIUDAD\n');
                 console.log('Ciudad: ', placeCho.nombre);
                 console.log('Latitud: ', placeCho.lat);
                 console.log('Longitud: ', placeCho.lng);
+                console.log('Tempreatura: ', clima.temp);
+                console.log('T. Mínima: ', clima.min);
+                console.log('T. Máxima: ', clima.max);
+                console.log('Cómo es el clima: ', clima.desc);
                 break;
 
                 case 2:
