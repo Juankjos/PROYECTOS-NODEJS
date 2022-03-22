142de13f2bf6f1850925f9427de6fa2c2a045fa1
Juankjos <juanca171097@gmail.com>
2022-03-22 13:33:57 -0600

    GOOGLE SIGIN

---
+++ curso-node-restserver-google-signin-updated/controllers/auth.js
@@ -0,0 +1,118 @@
+const { response } = require('express');
+const bcryptjs = require('bcryptjs')
+
+const Usuario = require('../models/usuario');
+
+const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
+
+
+const login = async(req, res = response) => {
+
+    const { correo, password } = req.body;
+
+    try {
+      
+        // Verificar si el email existe
+        const usuario = await Usuario.findOne({ correo });
+        if ( !usuario ) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - correo'
+            });
+        }
+
+        // SI el usuario está activo
+        if ( !usuario.estado ) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - estado: false'
+            });
+        }
+
+        // Verificar la contraseña
+        const validPassword = bcryptjs.compareSync( password, usuario.password );
+        if ( !validPassword ) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }   
+
+}
+
+//Crea el googleSignIn, que con una constante de token obtiene los datos de la página como un request
+const googleSignin = async(req, res = response) => {
+
+    const { id_token } = req.body;
+    
+    //Con los datos de correo, usuario y la imagen de perfil se toman los atributos para la verificación de Google
+    //Hasta que se encuentre el email
+    try {
+        const { correo, nombre, img } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        //Si el usuario es inexistente, se debe de crear. El password puede ser un vacío pero así está mejor
+        //Para indicar que debe llenarse con una contraseña
+        if ( !usuario ) {
+            // Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        // Si el usuario en DB
+        //Si el usuario no tiene estado de true se refleja un error de autenticación en el que como está en false,
+        //muestre este mensaje
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+        
+        res.json({
+            usuario,
+            token
+        });
+        
+    } catch (error) {
+
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        })
+
+    }
+
+
+
+}
+
+
+
+module.exports = {
+    login,
+    googleSignin
+}
